3540cbc9c99756bd954b4caeb1c13093671b7770
jafar <jafaralihajari@gmail.com>
2024-07-09 21:10:41 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/dashboard', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
+
+const dataSchema = new mongoose.Schema({}, { strict: false });
+
+const Data = mongoose.model('Data', dataSchema, 'dataCollection');
+
+app.get('/data', async (req, res) => {
+  try {
+    const data = await Data.find();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
